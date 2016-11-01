efa885aa87dd895c8efe627f3cf302fc378ddb4f
Tropic <tropical.productions.scratch@gmail.com>
2016-11-01 21:23:28 +0100

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,1 +1,15 @@
 var ai = [];
+
+function randomBetween(min, max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+function genRandGenes() {
+  return []; // WIP, will be changed
+}
+
+function genRandAI() {
+  var width = randomBetween(35, 55);
+  var height = randomBetween(35, 55);
+  ai.push([Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * (600 - width)), Math.floor(Math.random() * (600 - height)), width, height, Math.floor(Math.random() * 360), genRandGenes()]);
+}
