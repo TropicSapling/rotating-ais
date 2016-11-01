ddb8881c1333ba44a9f53f0b16ae83fa567c1b6d
Tropic <tropical.productions.scratch@gmail.com>
2016-11-01 21:23:07 +0100

    Update game.js

--- game.js
+++ game.js
@@ -16,20 +16,6 @@ function interval(func, wait, times){
   setTimeout(interv, wait);
 };
 
-function randomBetween(min, max) {
-  return Math.floor(Math.random()*(max-min+1)+min);
-}
-
-function genRandGenes() {
-  return []; // WIP, will be changed
-}
-
-function genRandAI() {
-  var width = randomBetween(35, 55);
-  var height = randomBetween(35, 55);
-  ai.push([Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * (600 - width)), Math.floor(Math.random() * (600 - height)), width, height, Math.floor(Math.random() * 360), genRandGenes()]);
-}
-
 function renderAIs(game) {
   for(i = 0; i < ai.length; i++) {
     if(ai[i][3] < 600 - ai[i][5]) {
