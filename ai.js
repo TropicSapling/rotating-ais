91c97559bc7878bbe268bd56a5953ed683da472e
Tropic <tropical.productions.scratch@gmail.com>
2016-11-01 18:53:59 +0100

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,1 +1,1 @@
-var ai = [[Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 600), Math.floor(Math.random() * 600), 50, 50, []]]; // Example, will be changed later
+var ai = [];
