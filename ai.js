66c68962b4c7413a1b0ddc72adcf50b7739f3ac4
Tropic <tropical.productions.scratch@gmail.com>
2016-11-02 12:03:13 +0100

    Update ai.js

--- ai.js
+++ ai.js
@@ -14,8 +14,13 @@ function genRandGenes(id) {
 function genRandAI() {
   var width = randomBetween(35, 55);
   var height = randomBetween(35, 55);
+  var placeAvailable = ai.indexOf("dead");
   
-  ai.push([Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * (600 - width)), Math.floor(Math.random() * (600 - height)), width, height, Math.floor(Math.random() * 360), genRandGenes(ai.length)]);
+  if(placeAvailable == -1) {
+    ai.push([Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * (600 - width)), Math.floor(Math.random() * (600 - height)), width, height, Math.floor(Math.random() * 360), genRandGenes(ai.length)]);
+  } else {
+    ai[placeAvailable] = [Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * (600 - width)), Math.floor(Math.random() * (600 - height)), width, height, Math.floor(Math.random() * 360), genRandGenes(ai.length)];
+  }
 
   total_ais++;
 }
