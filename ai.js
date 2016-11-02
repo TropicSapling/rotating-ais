ee438ea2577c044f45a6d067d0bb22af8872dd27
Tropic <tropical.productions.scratch@gmail.com>
2016-11-02 15:22:53 +0100

    Update ai.js

--- ai.js
+++ ai.js
@@ -7,8 +7,26 @@ function randomBetween(min, max) {
   return Math.floor(Math.random()*(max-min+1)+min);
 }
 
-function genRandGenes(id) {
-  return []; // WIP, will be changed later
+function genRandCond() {
+  var parenthesis = 0;
+  var op = 0;
+  
+  ai[id].push([]); // Add base for condition gene
+  ai[id].push(1); // Add base for the gene controlling the length of the condition gene
+  
+  do {
+    ai[id][9] = ai[id][9] + randomBetween(-3, 3); // [9] = where the length of condition gene is stored
+  } while(ai[id][9] % 2 == 0 && ai[id][9] > 0);
+  
+  for(i = 0; i < ai[id][9]; i++) {
+    findVal(op, parenthesis);
+    op++;
+  }
+  
+  while(parenthesis > 0) {
+    ai[id][8].push(")"); // [8] = where the condition gene is stored
+    parenthesis--;
+  }
 }
 
 function genRandAI() {
@@ -17,9 +35,11 @@ function genRandAI() {
   var placeAvailable = ai.indexOf("dead");
   
   if(placeAvailable == -1) {
-    ai.push([Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * (600 - width)), Math.floor(Math.random() * (600 - height)), width, height, Math.floor(Math.random() * 360), genRandGenes(ai.length)]);
+    ai.push([Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * (600 - width)), Math.floor(Math.random() * (600 - height)), width, height, Math.floor(Math.random() * 360)]);
+    genRandCond();
   } else {
-    ai[placeAvailable] = [Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * (600 - width)), Math.floor(Math.random() * (600 - height)), width, height, Math.floor(Math.random() * 360), genRandGenes(ai.length)];
+    ai[placeAvailable] = [Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * (600 - width)), Math.floor(Math.random() * (600 - height)), width, height, Math.floor(Math.random() * 360)];
+    genRandCond();
   }
 
   ais_alive++;
