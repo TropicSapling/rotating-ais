774add3d7461c5444957098c32876782583e920f
Tropic <tropical.productions.scratch@gmail.com>
2016-11-02 20:25:47 +0100

    Fixed bug **finally**...

--- game.js
+++ game.js
@@ -19,7 +19,7 @@ function interval(func, wait, times){
 };
 
 function checkCond(id) {
-/*  try {
+  try {
     func = new Function("return " + ai[id][8].join(" "));
     action = func();
     
@@ -29,8 +29,8 @@ function checkCond(id) {
   } catch(e) {
     ai[id].splice(8, 2);
     genRandCond(id);
-    checkCond();
-  } */
+    checkCond(id);
+  }
 }
 
 function renderAIs(game) {
