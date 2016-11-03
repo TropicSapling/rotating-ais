c67b503ae4af4bdc2ae26c3474909b671a86cefb
Tropic <tropical.productions.scratch@gmail.com>
2016-11-03 23:29:22 +0100

    Update game.js

--- game.js
+++ game.js
@@ -39,57 +39,57 @@ function renderAIs(game) {
   
   ai_sorted = ai_sorted.sort(function(a,b){return a - b});
   
-  for(i = 0; i < ai.length; i++) {
-    if(ai[i][10] && ai[i][10][0] == "dying") {
-      if(ai[i][5] > 1 && ai[i][6] > 1) {
-        ai[i][10][1] = ai[i][10][1] * 1.1;
-        var change = ai[i][10][1];
+  for(i = 0; i < ai_sorted.length; i++) {
+    if(ai_sorted[i][10] && ai_sorted[i][10][0] == "dying") {
+      if(ai_sorted[i][5] > 1 && ai_sorted[i][6] > 1) {
+        ai_sorted[i][10][1] = ai_sorted[i][10][1] * 1.1;
+        var change = ai_sorted[i][10][1];
         
-        ai[i][5] -= change;
-        ai[i][6] -= change;
+        ai_sorted[i][5] -= change;
+        ai_sorted[i][6] -= change;
         
-        ai[i][3] += change / 2;
-        ai[i][4] += change / 2;
+        ai_sorted[i][3] += change / 2;
+        ai_sorted[i][4] += change / 2;
       } else {
-        ai[i] = "dead";
+        ai_sorted[i] = "dead";
         ais_alive--;
       }
     }
     
-    if(ai[i] != "dead") {
-      if(ai[i][10] && ai[i][10][0] != "dying") {
-        if(ai[i][5] < ai[i][10][0] || ai[i][6] < ai[i][10][1]) { // [10][0] = full width, [10][1] = full height
-          var changeX = ai[i][5] * 1.1 - ai[i][5];
-          var changeY = ai[i][6] * 1.1 - ai[i][6];
+    if(ai_sorted[i] != "dead") {
+      if(ai_sorted[i][10] && ai_sorted[i][10][0] != "dying") {
+        if(ai_sorted[i][5] < ai_sorted[i][10][0] || ai_sorted[i][6] < ai_sorted[i][10][1]) { // [10][0] = full width, [10][1] = full height
+          var changeX = ai_sorted[i][5] * 1.1 - ai_sorted[i][5];
+          var changeY = ai_sorted[i][6] * 1.1 - ai_sorted[i][6];
           
-          ai[i][5] += changeX;
-          ai[i][6] += changeY;
+          ai_sorted[i][5] += changeX;
+          ai_sorted[i][6] += changeY;
           
-          ai[i][3] -= changeX / 2;
-          ai[i][4] -= changeY / 2;
+          ai_sorted[i][3] -= changeX / 2;
+          ai_sorted[i][4] -= changeY / 2;
         } else {
-          ai[i][5] = ai[i][10][0];
-          ai[i][6] = ai[i][10][1];
-          ai[i].splice(10, 1);
+          ai_sorted[i][5] = ai_sorted[i][10][0];
+          ai_sorted[i][6] = ai_sorted[i][10][1];
+          ai_sorted[i].splice(10, 1);
         }
-      } else if(!ai[i][10]) {
-        if(ai[i][3] < 600 - ai[i][5] && ai[i][3] > 0) { // [3] = x position, [5] = width
-          ai[i][3] += Math.sin(ai[i][7]); // [7] = rotation
-        }
-        
-        if(ai[i][4] < 600 - ai[i][6] && ai[i][4] > 0) { // [4] = y position, [6] = height
-          ai[i][4] += Math.cos(ai[i][7]);
+      } else if(!ai_sorted[i][10]) {
+        if(ai_sorted[i][3] < 600 - ai_sorted[i][5] && ai_sorted[i][3] > 0) { // [3] = x position, [5] = width
+          ai_sorted[i][3] += Math.sin(ai_sorted[i][7]); // [7] = rotation
         }
         
-        ai[i][5] -= 0.02;
-        ai[i][6] -= 0.02;
+        if(ai_sorted[i][4] < 600 - ai_sorted[i][6] && ai_sorted[i][4] > 0) { // [4] = y position, [6] = height
+          ai_sorted[i][4] += Math.cos(ai_sorted[i][7]);
+        }
+        
+        ai_sorted[i][5] -= 0.02;
+        ai_sorted[i][6] -= 0.02;
       }
       
-      if(ai[i][5] + ai[i][6] < 60 && !(ai[i][10])) {
-        ai[i].push(["dying", 1.1]);
+      if(ai_sorted[i][5] + ai_sorted[i][6] < 60 && !(ai_sorted[i][10])) {
+        ai_sorted[i].push(["dying", 1.1]);
       } else {
-        game.fillStyle = "rgb(" + ai[i][0] + ", " + ai[i][1] + ", " + ai[i][2] + ")"; // [0], [1] and [2] are colour values
-        game.fillRect(ai[i][3], ai[i][4], ai[i][5], ai[i][6]);
+        game.fillStyle = "rgb(" + ai_sorted[i][0] + ", " + ai_sorted[i][1] + ", " + ai_sorted[i][2] + ")"; // [0], [1] and [2] are colour values
+        game.fillRect(ai_sorted[i][3], ai_sorted[i][4], ai_sorted[i][5], ai_sorted[i][6]);
       }
     }
   }
