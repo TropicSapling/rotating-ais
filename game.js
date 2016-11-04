1d30c2425006cc02bbef0815dd211abcb71f0ef3
Tropic <tropical.productions.scratch@gmail.com>
2016-11-04 18:47:01 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 window.onerror = function(msg, url, line, column, error) {
-  alert(msg + " in file " + url + "\n\nLine: " + line + ", column: " + column + "\n\nStack Trace:\n" + error);
+  alert(msg + " in file " + url + "\n\nLine: " + line + ", column: " + column + "\n\nStack Trace:\n" + error.stack);
 }
 
 function interval(func, wait, times){
