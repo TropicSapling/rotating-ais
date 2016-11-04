ac5acc54ea277630de7cc70bde12ab2523cc74a8
Tropic <tropical.productions.scratch@gmail.com>
2016-11-04 22:16:18 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,55 +1,8 @@
-function getStackTrace() {
-  var callstack = [];
-  var isCallstackPopulated = false;
-  try {
-    i.dont.exist+=0; //doesn't exist- that's the point
-  } catch(e) {
-    if (e.stack) { //Firefox
-      var lines = e.stack.split('\n');
-      for (var i=0, len=lines.length; i&lt;len; i++) {
-        if (lines[i].match(/^\s*[A-Za-z0-9\-_\$]+\(/)) {
-          callstack.push(lines[i]);
-        }
-      }
-      //Remove call to printStackTrace()
-      callstack.shift();
-      isCallstackPopulated = true;
-    }
-    else if (window.opera &amp;&amp; e.message) { //Opera
-      var lines = e.message.split('\n');
-      for (var i=0, len=lines.length; i&lt;len; i++) {
-        if (lines[i].match(/^\s*[A-Za-z0-9\-_\$]+\(/)) {
-          var entry = lines[i];
-          //Append next line also since it has the file info
-          if (lines[i+1]) {
-            entry += ' at ' + lines[i+1];
-            i++;
-          }
-          callstack.push(entry);
-        }
-      }
-      //Remove call to printStackTrace()
-      callstack.shift();
-      isCallstackPopulated = true;
-    }
-  }
-  if (!isCallstackPopulated) { //IE and Safari
-    var currentFunction = arguments.callee.caller;
-    while (currentFunction) {
-      var fn = currentFunction.toString();
-      var fname = fn.substring(fn.indexOf(&amp;quot;function&amp;quot;) + 8, fn.indexOf('')) || 'anonymous';
-      callstack.push(fname);
-      currentFunction = currentFunction.caller;
-    }
-  }
-  return callstack;
-}
-   
 window.onerror = function(msg, url, line, column, error) {
   if(error) {
     alert(msg + " in file " + url + "\n\nLine: " + line + ", column: " + column + "\n\nStack Trace:\n" + error.stack);
   } else {
-    alert(msg + " in file " + url + "\n\nLine: " + line + ", column: " + column + "\n\nStack Trace:\n" + getStackTrace());
+    alert(msg + " in file " + url + "\n\nLine: " + line + ", column: " + column);
   }
   
   alert("[" + ai.join("],\n\n[") + "]");
