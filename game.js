11311d0781e43624a2cbb51bc1997d9fae403402
Tropic <tropical.productions.scratch@gmail.com>
2016-11-05 22:20:12 +0100

    Update game.js

--- game.js
+++ game.js
@@ -143,7 +143,7 @@ function findCollision(id, taken) {
     var h1 = ai[id][6];
     var h2 = ai[i][6];
     
-    if(taken.indexOf(i) == -1 && (id == i || ((x1 <= x2 + w2 && x1 + w1 >= x2 && y1 <= y2 + h2 && y1 + h1 >= y2) && (x1 - x2 < 15)))) {
+    if(taken.indexOf(i) == -1 && (id == i || ((x1 <= x2 + w2 && x1 + w1 >= x2 && y1 <= y2 + h2 && y1 + h1 >= y2) && (x1 - x2 < 15 && x1 + w1 - x2 - w2 > -15)))) {
       collisions.push(i);
     }
   }
