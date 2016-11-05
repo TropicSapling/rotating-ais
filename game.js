afff0a1bbf7aeeb7d84edb1e089d764797417073
Tropic <tropical.productions.scratch@gmail.com>
2016-11-05 22:13:22 +0100

    Update game.js

--- game.js
+++ game.js
@@ -143,7 +143,7 @@ function findCollision(id, taken) {
     var h1 = ai[id][6];
     var h2 = ai[i][6];
     
-    if(taken.indexOf(i) == -1 && (id == i || (Math.round(x1 / 10) == Math.round(x2 / 10) && Math.round((x1 + w1) / 10) == Math.round((x2 + w2) / 10) && Math.round(y1 / 10) == Math.round(y2 / 10) && Math.round((y1 + h1) / 10) == Math.round((y2 + h2) / 10)))) {
+    if(taken.indexOf(i) == -1 && (id == i || (x1 <= x2 + w2 && x1 + w1 >= x2 && y1 <= y2 + h2 && y1 + h1 >= y2))) {
       collisions.push(i);
     }
   }
@@ -190,8 +190,6 @@ function checkCollisions(game) {
           ai[collidingAIs[i][j]] = "dead";
           ai[collidingAIs[i][biggestAI]][5] += Math.sqrt(size);
           ai[collidingAIs[i][biggestAI]][6] += Math.sqrt(size);
-          alert("[" + ai.join("],\n\n[") + "]");
-          alert(collidingAIs[i][biggestAI]);
         }
       }
     }
