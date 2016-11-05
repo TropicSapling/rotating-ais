f86bf0e99263e1e48b3182d5f1154f927b05c570
Tropic <tropical.productions.scratch@gmail.com>
2016-11-05 20:42:10 +0100

    Update game.js

--- game.js
+++ game.js
@@ -134,7 +134,7 @@ function findCollision(id) {
   var collisions = [];
   
   for(i = 0; i < ai.length; i++) {
-    if(Math.round(ai[id][3] / 10) == Math.round(ai[i][3] / 10) && Math.round(ai[id][4] / 10) == Math.round(ai[i][4] / 10)) {
+    if(id == i || (Math.round(ai[id][3] / 10) == Math.round(ai[i][3] / 10) && Math.round(ai[id][4] / 10) == Math.round(ai[i][4] / 10))) {
       collisions.push(i);
     }
   }
