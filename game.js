a81dac5534390eb23d913bc6f42abc2b8a3632cf
Tropic <tropical.productions.scratch@gmail.com>
2016-11-06 22:51:44 +0100

    Update game.js

--- game.js
+++ game.js
@@ -185,6 +185,10 @@ function checkCollisions(game) {
         }
       }
       
+      alert("Colliding: " + collidingAIs[i].join(", "));
+      alert("Biggest: " + biggestAI);
+      alert("Same sizes: " + sameAIs[i].join(", "));
+      
       for(j = 0; j < collidingAIs[i].length; j++) {
         if(j !== biggestAI && biggestAI != -1) {
           var size = ai[collidingAIs[i][j]][5] * ai[collidingAIs[i][j]][6];
