71fe489cf135f66c20b61c1db61d1e7d2c80a142
Tropic <tropical.productions.scratch@gmail.com>
2016-11-07 14:48:14 +0100

    Update ai.js

--- ai.js
+++ ai.js
@@ -10,23 +10,25 @@ function randomBetween(min, max) {
 }
 
 Array.prototype.equals = function (array) {
-    if (!array)
-        return false;
-
-    if (this.length != array.length)
-        return false;
-
-    for (var i = 0, l=this.length; i < l; i++) {
-        if (this[i] instanceof Array && array[i] instanceof Array) {
-            if (!this[i].equals(array[i]))
-                return false;       
-        }           
-        else if (this[i] != array[i]) { 
-            return false;   
-        }           
-    }       
-    return true;
+  if (!array)
+      return false;
+  
+  if (this.length != array.length)
+    return false;
+  
+  for (var i = 0, l=this.length; i < l; i++) {
+    if (this[i] instanceof Array && array[i] instanceof Array) {
+      if (!this[i].equals(array[i]))
+        return false;       
+      }           
+      else if (this[i] != array[i]) { 
+        return false;   
+      }           
+  }       
+  return true;
 }
+
+Object.defineProperty(Array.prototype, "equals", {enumerable: false});
 
 function findInput(id) {
   var randParenthesis = Math.round(Math.random());
