6e89e62b4e1f0890e7cde968067e2f7331a3e95f
Tropic <tropical.productions.scratch@gmail.com>
2016-11-07 17:27:48 +0100

    Update ai.js

--- ai.js
+++ ai.js
@@ -86,7 +86,7 @@ function genRandGenes() {
 function combineGenes(par1, par2) {
   var placeAvailable = ai.indexOf("dead");
   
-  if(par1 == par2) {
+  if(par1 == par2 || true) { // Obviously "|| true" will be removed later, just there now because I'm not done with the "else" part
     var width = ai[par1][5];
     var height = ai[par1][6];
     
