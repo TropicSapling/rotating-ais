ea7e912493e4949b662461a85172d1484a9fb17b
Tropic <tropical.productions.scratch@gmail.com>
2016-11-11 18:30:54 +0100

    Bugfixes

--- game.js
+++ game.js
@@ -145,7 +145,7 @@ function findCollision(id, taken) {
     var h1 = ai[id][6];
     var h2 = ai[i][6];
     
-    if(taken.indexOf(i) == -1 && (id == i || (x1 - x2 < 10 && x1 + w1 - x2 - w2 > -10 && y1 - y2 < 10 && y1 + h1 - y2 - h2 > -10))) {
+    if(taken.indexOf(i) == -1 && (id == i || (x1 <= x2 + w2 && x1 + w1 >= x2 && y1 <= y2 + h2 && y1 + h1 >= y2))) {
       collisions.push(i);
     }
   }
@@ -174,13 +174,21 @@ function checkCollisions(game) {
     if(collidingAIs[i].length > 1) {
       for(j = 0; j < collidingAIs[i].length; j++) {
         var size = ai[collidingAIs[i][j]][5] * ai[collidingAIs[i][j]][6];
+        var x1 = ai[collidingAIs[i][j]][3];
+        var y1 = ai[collidingAIs[i][j]][4];
+        var w1 = ai[collidingAIs[i][j]][5];
+        var h1 = ai[collidingAIs[i][j]][6];
         
         sameAIs[i].push([]);
         
         for(k = 0; k < collidingAIs[i].length; k++) {
           var size2 = ai[collidingAIs[i][k]][5] * ai[collidingAIs[i][k]][6];
+          var x1 = ai[collidingAIs[i][k]][3];
+          var y1 = ai[collidingAIs[i][k]][4];
+          var w1 = ai[collidingAIs[i][k]][5];
+          var h1 = ai[collidingAIs[i][k]][6];
           
-          if(Math.sqrt(size2) / Math.sqrt(size) < 0.9) {
+          if((x1 - x2 < 10 && x1 + w1 - x2 - w2 > -10 && y1 - y2 < 10 && y1 + h1 - y2 - h2 > -10) && Math.sqrt(size2) / Math.sqrt(size) < 0.9) {
             ai[collidingAIs[i][k]] = "dead";
             ais_alive--;
             
