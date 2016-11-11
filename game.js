f6662d21bfc340f8b802d67631d36dc1f3a1678e
Tropic <tropical.productions.scratch@gmail.com>
2016-11-11 18:13:58 +0100

    Removed unnecessary code ruining everything

--- game.js
+++ game.js
@@ -145,7 +145,7 @@ function findCollision(id, taken) {
     var h1 = ai[id][6];
     var h2 = ai[i][6];
     
-    if(taken.indexOf(i) == -1 && (id == i || ((x1 <= x2 + w2 && x1 + w1 >= x2 && y1 <= y2 + h2 && y1 + h1 >= y2) && (x1 - x2 < 10 && x1 + w1 - x2 - w2 > -10 && y1 - y2 < 10 && y1 + h1 - y2 - h2 > -10)))) {
+    if(taken.indexOf(i) == -1 && (id == i || (x1 - x2 < 10 && x1 + w1 - x2 - w2 > -10 && y1 - y2 < 10 && y1 + h1 - y2 - h2 > -10))) {
       collisions.push(i);
     }
   }
