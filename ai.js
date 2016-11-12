de38e25d76d840b2906bd2224f0fbf3b32f159e9
Tropic <tropical.productions.scratch@gmail.com>
2016-11-12 18:50:13 +0100

    Bugfixes

--- ai.js
+++ ai.js
@@ -182,7 +182,7 @@ function combineGenes(par1, par2) {
           ai[ai.length - 1].push(ai[par1][i]);
         } else if(i == 8) {
           combineConditions(ai.length - 1, par1[8], par2[8]);
-        } else if(typeof ai[par1][i] === 'number') {
+        } else if(typeof ai[par1][i] === 'number' && i != 9) {
           ai[ai.length - 1].push(randomBetween(Math.min(ai[par1][i], ai[par2][i]), Math.max(ai[par1][i], ai[par2][i])));
         }
       }
@@ -205,7 +205,7 @@ function combineGenes(par1, par2) {
           ai[placeAvailable].push(ai[par1][i]);
         } else if(i == 8) {
           combineConditions(placeAvailable, par1[8], par2[8]);
-        } else if(typeof ai[par1][i] === 'number') {
+        } else if(typeof ai[par1][i] === 'number' && i != 9) {
 	  ai[placeAvailable].push(randomBetween(Math.min(ai[par1][i], ai[par2][i]), Math.max(ai[par1][i], ai[par2][i])));
         }
       }
