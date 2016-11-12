6e20f84b8b686dab08f437a2557b74ff24565876
Tropic <tropical.productions.scratch@gmail.com>
2016-11-12 16:18:43 +0100

    Fixed some AIs growing enormous

--- ai.js
+++ ai.js
@@ -215,7 +215,7 @@ function combineGenes(par1, par2) {
     }
   } else {
     for(var i = 0; i < ai[placeAvailable].length; i++) {
-      if(Math.floor(Math.random() * (1 / mutation_chance)) == 0) {
+      if(i != 5 && i != 6 && Math.floor(Math.random() * (1 / mutation_chance)) == 0) {
         if(typeof ai[placeAvailable][i] === 'number') {
           if(Math.round(Math.random())) {
             ai[placeAvailable][i] += 2;
