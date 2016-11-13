4663af17d0f01023a7caf87dada33ccfb72eab36
Tropic <tropical.productions.scratch@gmail.com>
2016-11-13 01:04:31 +0100

    Bugfixes

--- game.js
+++ game.js
@@ -112,13 +112,13 @@ function renderAIs(game) {
       if(ai_sorted[i][3] < 0) {
         ai_sorted[i][3] = 0;
         ai_sorted[i][7] = Math.floor(Math.random() * 365);
-      } else if(ai_sorted[i][3] > 600) {
+      } else if(ai_sorted[i][3] > 600 - ai_sorted[i][5]) {
         ai_sorted[i][3] = 600;
         ai_sorted[i][7] = Math.floor(Math.random() * 365);
       } else if(ai_sorted[i][4] < 0) {
         ai_sorted[i][4] = 0;
         ai_sorted[i][7] = Math.floor(Math.random() * 365);
-      } else if(ai_sorted[i][4] > 600) {
+      } else if(ai_sorted[i][4] > 600 - ai_sorted[i][6]) {
         ai_sorted[i][4] = 600;
         ai_sorted[i][7] = Math.floor(Math.random() * 365);
       }
