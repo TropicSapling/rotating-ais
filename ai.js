5a7be367f89ebf2519e21544c761e1fd70e5edb9
Tropic <tropical.productions.scratch@gmail.com>
2016-11-14 20:42:31 +0100

    Increased condition gene length

--- ai.js
+++ ai.js
@@ -76,9 +76,9 @@ function findInput(id) {
 
 function genRandCond(id) {
   ai[id].push([]); // Add base for condition gene
-  ai[id].push(randomBetween(2, 10)); // Add base for the gene controlling the length of the condition gene
+  ai[id].push(randomBetween(2, 16)); // Add base for the gene controlling the length of the condition gene
   while(ai[id][9] % 2 == 0) {
-    ai[id][9] = randomBetween(2, 10);
+    ai[id][9] = randomBetween(2, 16);
   }
   
   if(Math.round(Math.random())) {
