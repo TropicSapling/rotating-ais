20bafc75b9f1c60ac373ef971cc8db4b1472a970
Tropic <tropical.productions.scratch@gmail.com>
2016-11-14 20:57:05 +0100

    Testing death on edge again

--- game.js
+++ game.js
@@ -143,17 +143,25 @@ function renderAIs(game) {
       }
       
       if(ai_sorted[i][3] < 0) {
-        ai_sorted[i][3] = 0;
-        ai_sorted[i][7] = Math.floor(Math.random() * 365);
+//        ai_sorted[i][3] = 0;
+//        ai_sorted[i][7] = Math.floor(Math.random() * 365);
+        ai_sorted[i] = "dead";
+        checked_ais.splice(checked_ais.indexOf(i));
       } else if(ai_sorted[i][3] > 600 - ai_sorted[i][5]) {
-        ai_sorted[i][3] = 600 - ai_sorted[i][5];
-        ai_sorted[i][7] = Math.floor(Math.random() * 365);
+//        ai_sorted[i][3] = 600 - ai_sorted[i][5];
+//        ai_sorted[i][7] = Math.floor(Math.random() * 365);
+        ai_sorted[i] = "dead";
+        checked_ais.splice(checked_ais.indexOf(i));
       } else if(ai_sorted[i][4] < 0) {
-        ai_sorted[i][4] = 0;
-        ai_sorted[i][7] = Math.floor(Math.random() * 365);
+//        ai_sorted[i][4] = 0;
+//        ai_sorted[i][7] = Math.floor(Math.random() * 365);
+        ai_sorted[i] = "dead";
+        checked_ais.splice(checked_ais.indexOf(i));
       } else if(ai_sorted[i][4] > 600 - ai_sorted[i][6]) {
-        ai_sorted[i][4] = 600 - ai_sorted[i][6];
-        ai_sorted[i][7] = Math.floor(Math.random() * 365);
+//        ai_sorted[i][4] = 600 - ai_sorted[i][6];
+//        ai_sorted[i][7] = Math.floor(Math.random() * 365);
+        ai_sorted[i] = "dead";
+        checked_ais.splice(checked_ais.indexOf(i));
       }
       
       if(ai_sorted[i][5] * ai_sorted[i][6] < 100 && (!(ai_sorted[i][10]) || (ai_sorted[i][10] && typeof ai_sorted[i][10][0] === 'object'))) {
