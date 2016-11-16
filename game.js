3170d19f489fdabb328b4019a448ad59fb75cc71
Tropic <tropical.productions.scratch@gmail.com>
2016-11-16 21:46:43 +0100

    Update game.js

--- game.js
+++ game.js
@@ -15,6 +15,8 @@ window.onerror = function(msg, url, line, column, error) {
 		console.log("[!] Error: " + msg + " in file " + url);
 		console.log("Line: " + line + ", column: " + column);
 	}
+	
+	clearInterval(gameLoop);
 }
 
 function deepCopy(arr) { // Because JS hates me and is just that annoying
