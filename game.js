b7cbcbcc71f3211e9696093628db5c9df5707c8b
Tropic <tropical.productions.scratch@gmail.com>
2016-11-17 21:07:11 +0100

    Update game.js

--- game.js
+++ game.js
@@ -300,11 +300,7 @@ $(function() {
 		
 		for(i = 0; i < ai.length; i++) {
 			if(ai[i] !== "dead" && (!(ai[i][10]) || (ai[i][10] && typeof ai[i][10][0] === 'object'))) {
-				console.log(ai[i]);
-				console.log(i);
 				checkCond(i);
-				console.log(ai[i]);
-				console.log(i);
 				
 				if(time_alive[i] > longest_alive[0]) {
 					longest_alive = [time_alive[i], ai[i]];
