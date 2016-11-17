ca228df92760c826328ad2d33d7a0887002aba8f
Tropic <tropical.productions.scratch@gmail.com>
2016-11-17 19:52:42 +0100

    Update game.js

--- game.js
+++ game.js
@@ -304,8 +304,14 @@ $(function() {
 				
 				if(time_alive[i] > longest_alive[0]) {
 					longest_alive = [time_alive[i], ai[i]];
+					console.log("WHOLE: " + !(ai[i][10]) || (ai[i][10] && typeof ai[i][10][0] === 'object'));
+					console.log("FIRST: " + !(ai[i][10]));
+					console.log("SECOND: " + ai[i][10] && typeof ai[i][10][0] === 'object');
 				} else if(time_alive[i] > longest_alive_2nd[0]) {
 					longest_alive_2nd = [time_alive[i], ai[i]];
+					console.log("WHOLE: " + !(ai[i][10]) || (ai[i][10] && typeof ai[i][10][0] === 'object'));
+					console.log("FIRST: " + !(ai[i][10]));
+					console.log("SECOND: " + ai[i][10] && typeof ai[i][10][0] === 'object');
 				}
 			}
 			
