9ef3984ade0768abe75b99f050106f2d6785ff2e
Tropic <tropical.productions.scratch@gmail.com>
2016-11-19 22:14:36 +0100

    Bugfixes

--- game.js
+++ game.js
@@ -372,7 +372,7 @@ $(function() {
 			
 			var pos = 0;
 			if(time_alive_sorted.length > 20) {
-				while(time_alive[pos][2] || time_alive[pos][2] > 10000) {
+				while(time_alive[pos][2] || time_alive[pos][0] > 10000) {
 					pos++;
 				}
 				
