580f6b18268965c582b506e01240d7d9d151ce00
Tropic <tropical.productions.scratch@gmail.com>
2016-11-20 14:24:56 +0100

    Bugfixes & fixed indentation

--- game.js
+++ game.js
@@ -38,43 +38,58 @@ function regenCond(id) {
 	}
 }
 
+function getCondGene(cond) {
+	var processed_cond = "";
+	for(part = 0; part < cond.length; part++) {
+		var code = cond[part];
+		if(typeof code === 'object') {
+			processed_cond += code[0];
+		} else {
+			processed_cond += code;
+		}
+	}
+	
+	return processed_cond;
+}
+
 function checkCond(id) {
 	try {
-		func = new Function("return " + ai[id][8].join(" "));
+		var condGene = getCondGene(ai[id][8]);
+		func = new Function("return " + condGene.join(" "));
 		var action = func();
 		
 		if(checked_ais.indexOf(id) == -1) {
 			var condIsConst = true;
-			var cond = ai[id][8].join(" ");
+			var cond = condGene.join(" ");
 			for(var i = 0; i < changing_inputs.length; i++) {
 				if(cond.indexOf(changing_inputs[i]) != -1) {
 					condIsConst = false;
 				}
 			}
 			
-                        var repeats = 0;
+			var repeats = 0;
 			while(condIsConst && repeats < 1000) {
 				regenCond(id);
-				cond = ai[id][8].join(" ");
+				cond = getCondGene(ai[id][8]).join(" ");
 				
 				for(var i = 0; i < changing_inputs.length; i++) {
 					if(cond.indexOf(changing_inputs[i]) != -1) {
 						condIsConst = false;
 					}
 				}
-                                repeats++;
+				repeats++;
 			}
                         
 			if(repeats < 1000) {
-			        checked_ais.push(id);
-			        checkCond(id);
-                        } else {
-                                if(ai[id][10]) {
+				checked_ais.push(id);
+				checkCond(id);
+			} else {
+				if(ai[id][10]) {
 					ai[id].splice(10, 0, ["dying", 1.1]);
 				} else {
 					ai[id].push(["dying", 1.1]);
 				}
-                        }
+			}
 		} else if(action == true) {
 			ai[id][7] += 0.1;
 		}
@@ -389,7 +404,7 @@ $(function() {
 		renderAIs(game);
 		
 		if(time_alive_sorted.length > 0) {
-			$('#best-thought').html("<strong>Thoughts of the longest survivor:</strong> " + time_alive_sorted[0][1][8].join(" "));
+			$('#best-thought').html("<strong>Thoughts of the longest survivor:</strong> " + getCondGene(time_alive_sorted[0][1][8]).join(" "));
 		}
 	}, 20);
 });
