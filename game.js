a63ca84d46fb5313a66d83bd5a16d77e2da882cd
Tropic <tropical.productions.scratch@gmail.com>
2016-11-23 17:28:17 +0100

    Rotating now takes energy

--- game.js
+++ game.js
@@ -95,6 +95,10 @@ function checkCond(id) {
 			}
 		} else if(action == true) {
 			ai[id][7] += 0.2;
+			ai[id][5] -= ai[id][5] * 0.0008;
+			ai[id][3] += ai[id][5] * 0.0004;
+			ai[id][6] -= ai[id][6] * 0.0008;
+			ai[id][4] += ai[id][6] * 0.0004;
 		}
 	} catch(e) {
 		regenCond(id);
