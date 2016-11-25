d7cf78a12f03393a7a4dd18a6aacaf9d0a4362a0
Tropic <tropical.productions.scratch@gmail.com>
2016-11-25 21:47:04 +0100

    Mutation chance now slowly decreases by time
    Some examples:
    - 1 min: 97% of original
    - 5 min: 86% of original
    - 30 min: 41% of original
    - 1 hour: 17% of original
    - ~3 hours or longer: 0.7% of original (cap)

--- game.js
+++ game.js
@@ -419,5 +419,9 @@ $(function() {
 		if(time_alive_sorted.length > 0) {
 			$('#best-thought').html("<strong>Thoughts of the longest survivor:</strong> " + getCondGene(time_alive_sorted[0][1][8]).join(" "));
 		}
+		
+		if(performance.now() - start_time < 10000000) {
+			mutation_chance = mutation_chance * 0.9999;
+		}
 	}, 20);
 });
