99ad082a1df7361425ea616af470b40ee33c2d3c
Tropic <tropical.productions.scratch@gmail.com>
2016-11-27 19:48:40 +0100

    Decreased maximum total mass

--- game.js
+++ game.js
@@ -413,7 +413,7 @@ $(function() {
 			game.fillStyle = "#eee";
 			game.fillRect(0, 0, 600, 600); // Background
 			
-			if(total_mass < 20000) {
+			if(total_mass < 15000) {
 				if(ai.length > 1 && performance.now() - start_time > 5000 && Math.floor(Math.random() * (1 / rand_spawn_chance)) > 0) {
 					var par1 = findPar();
 					var par2 = findPar();
