eb3c8e5206f4b72d9f43d39c6654e9b9bae5120d
Tropic <tropical.productions.scratch@gmail.com>
2016-11-27 14:31:17 +0100

    Update game.js

--- game.js
+++ game.js
@@ -354,9 +354,11 @@ function findPar() {
 	}
 	
 	var randNumber = Math.floor(Math.random() * total_time_alive);
+	console.log(par_chance);
 	for(var i = 0; i < par_chance; i++) {
+		console.log(i);
+		console.log(time_alive[i]);
 		if(randNumber >= par_chance[i][0] && randNumber < par_chance[i][1]) {
-			console.log(time_alive[i]);
 			return time_alive[i][1];
 		}
 	}
