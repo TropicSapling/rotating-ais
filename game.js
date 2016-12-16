5d8dd89c48442b1e0f6f06792c41268b46d5c074
Tropic <tropical.productions.scratch@gmail.com>
2016-12-16 20:24:21 +0100

    ihatejs.com

--- game.js
+++ game.js
@@ -374,7 +374,7 @@ function getBrowserSpeed() {
 	
 	while(performance.now() - s_time < 200) {
 		var before_calc = performance.now();
-		for(i = 0; i < 1000; i++) {
+		for(var i = 0; i < 1000; i++) {
 			if(Math.round(Math.random())) {
 				test = test * 0.123456789;
 			} else {
@@ -386,7 +386,7 @@ function getBrowserSpeed() {
 	}
 	
 	var total_time = 0;
-	for(i = 0; i < time_to_calc.length; i++) {
+	for(var i = 0; i < time_to_calc.length; i++) {
 		total_time += time_to_calc[i];
 	}
 	
