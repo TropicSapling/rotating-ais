c3017d7f43e59e80b798210a7f7a5de04b54bd5a
Tropic <tropical.productions.scratch@gmail.com>
2017-01-30 21:52:57 +0100

    Performance Improvements

--- ai.js
+++ ai.js
@@ -123,9 +123,9 @@ function genRandConditions(id) {
 	ai[id].push([]); // Add base for condition length genes
 	
 	for(var action = 0; action < 2; action++) {
-		ai[id][9].push(randomBetween(4, 16));
+		ai[id][9].push(randomBetween(4, 8));
 		while(ai[id][9][action] % 2 == 0) {
-			ai[id][9][action] = randomBetween(4, 16);
+			ai[id][9][action] = randomBetween(4, 8);
 		}
 		
 		if(Math.round(Math.random())) {
