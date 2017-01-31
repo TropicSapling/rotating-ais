0f982cb0ca99c6d5d5814f4668404e88538cffb1
Tropic <tropical.productions.scratch@gmail.com>
2017-01-31 20:28:45 +0100

    Bugfixes

--- game.js
+++ game.js
@@ -38,7 +38,7 @@ function deepCopy(arr) { // Because JS hates me and is just that annoying
 function regenConditions(id) {
 	ai[id].splice(8, 2); // NOTE: After this splice, [8] = old [10]
 	if(ai[id][8]) {
-		combineConditions(id, ai[id][8][0][8], ai[id][8][1][8]);
+		combineConditions(id, deepCopy(ai[id][8][0][8]), deepCopy(ai[id][8][1][8]));
 	} else {
 		genRandConditions(id);
 	}
